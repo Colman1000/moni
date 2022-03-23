b29a08cd070bbe8ada500add36ae979cd2dbe669
Enger Colman <engerterseer@gmail.com>
2022-03-23 10:40:50 +0100

    ft - changed icon launcher

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,5 +1,7 @@
 class Assets {
   const Assets._(); // this basically makes it so you can't instantiate this class
+
   static const String backgroundImage = 'assets/images/BG.png';
+  static const String icon = 'assets/icon.png';
 
 }
